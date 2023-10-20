3a515c950f31f84ad7c54dd1f486c3bc4e47185d
reem00saab <134811073+reem00saab@users.noreply.github.com>
2023-10-20 14:21:34 +0300

    Add files via upload

---
+++ Reem/script.js
@@ -0,0 +1,6 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+});
